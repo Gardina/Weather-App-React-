667c3d9b5be032ca3b5105d3d7c46ea03020d043
Gardina <andrey.diving@gmail.com>
2017-05-27 21:59:35 +0700

    fix process.env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const PORT = process.evn.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
 	if (req.headers['x-forwarded-proto'] === 'http') {
