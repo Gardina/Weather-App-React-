3fc33ab19f690f0b60bac04c3e85c721c25518ee
Gardina <andrey.diving@gmail.com>
2017-05-28 00:15:51 +0700

    add grid to main component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+const {Link} = require('react-router');
 
 const Examples = (props) => {
 	return (
-      <h3>Examples Component</h3>
+    <div>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few examples to try out:</p>
+      <ol>
+        <li>
+          <Link to='/?location=St.Petersburg'>St. Petersburg, Russia</Link>
+        </li>
+        <li>
+          <Link to='/?location=Rio'>Rio, Brazil</Link>
+        </li>
+      </ol>
+    </div>
 	);
 };
 
